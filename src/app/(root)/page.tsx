fa6fa29e779dab974565e76f14e79398756a55d1
kishanghosh090 <kishanghosh090@gmail.com>
2026-09-09 01:10:00 +0530

    add css

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@base-ui/react'
 import React from 'react'
 
 function Home() {
   return (
-    <div className='bg-neutral-900 flex justify-center items-center h-screen '>
-      <h1 className='text-amber-50'>Home</h1>
+    <div className=' flex justify-center items-center  '>
+      <h1 className='text-amber-50'>
+        <Button>click me</Button>
+      </h1>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider,  } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,12 +22,22 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <ClerkProvider>
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+      <html
+        suppressHydrationWarning
+        lang="en"
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
