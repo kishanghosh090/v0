79fd263ab43b0616b1066e809e66cc2ce70afe30
kishanghosh090 <kishanghosh090@gmail.com>
2026-09-09 01:19:45 +0530

    add home

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,40 @@
-import { Button } from '@base-ui/react'
-import React from 'react'
+import { Button } from "@/components/ui/button";
 
-function Home() {
+import ProjectsForm from "@/modules/home/components/project-form";
+import ProjectList from "@/modules/home/components/project-list";
+import Image from "next/image";
+import React from "react";
+
+const Page = () => {
   return (
-    <div className=' flex justify-center items-center  '>
-      <h1 className='text-amber-50'>
-        <Button>click me</Button>
-      </h1>
-    </div>
-  )
-}
+    <div className="flex items-center justify-center w-full px-4 py-8">
+      <div className="max-w-5xl w-full">
+        <section className="space-y-8 flex flex-col items-center">
+          <div className="flex flex-col items-center">
+            <Image
+              src={"/logo.svg"}
+              width={100}
+              height={100}
+              alt="Logo"
+              className="hidden md:block invert dark:invert-0"
+            />
+          </div>
+          <h1 className="text-2xl md:text-5xl font-bold text-center">
+            Build Something with 💓
+          </h1>
 
-export default Home
+          <p className="text-lg md:text-xl text-muted-foreground text-center">
+            Create apps and websites by chatting with AI
+          </p>
+
+          <div className="max-w-3xl w-full">
+            <ProjectsForm />
+          </div>
+          <ProjectList />
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
